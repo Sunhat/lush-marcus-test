9260bcf6a0c796401b14a4ae425525ff0816c160
Marcus <Sunhat@users.noreply.github.com>
2023-07-27 21:40:14 +0100

    Fix prefix 'From'

--- src/hooks/usePricingText.ts
+++ src/hooks/usePricingText.ts
@@ -6,7 +6,7 @@ const CURRENCIES = {
 }
 export default function usePricingText(priceRange: PriceRange, quantity: number = 1) {
     return useMemo(() => {
-        const prefix = (priceRange.start.gross.amount === priceRange.stop.gross.amount) ?
+        const prefix = (priceRange.start.gross.amount !== priceRange.stop.gross.amount) ?
             'From ' : ''
         return prefix + `${CURRENCIES[priceRange.start.gross.currency]}${priceRange.start.gross.amount * quantity}`
     }, [priceRange, quantity])
